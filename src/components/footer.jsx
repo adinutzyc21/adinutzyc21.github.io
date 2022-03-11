ec91db5c89d5c50328e96fe585378e194f782ddb
Adina Stoica <adina.stoica@gmail.com>
2022-03-10 23:35:13 -0500

    Copyright link fix

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -5,7 +5,7 @@ export default function Footer() {
         <Paper style={{ color: "gray", position: "fixed", bottom: 0, width: "100%", zIndex: "9999", borderTop: "solid", height: "2rem" }} >
             <Typography variant="body2" color="textSecondary" align="center" style={{ lineHeight: "2rem" }}>
                 {'Copyright © '}
-                <Link color="inherit" href="www.adinastoica.com">
+                <Link color="inherit" href="/about">
                     Adina Raluca Stoica
                 </Link>{' '}
                 {new Date().getFullYear()}
