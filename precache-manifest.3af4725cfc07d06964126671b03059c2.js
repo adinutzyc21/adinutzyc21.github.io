fe36b05b7fd4d9789a20d9d37a73cc1af76643ef
Adina Stoica <adina.stoica@gmail.com>
2022-11-02 23:20:05 -0400

    Updates

---
+++ precache-manifest.3af4725cfc07d06964126671b03059c2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "882f6f6275676c4af169",
+    "url": "/static/css/main.d64d60c7.chunk.css"
+  },
+  {
+    "revision": "882f6f6275676c4af169",
+    "url": "/static/js/main.882f6f62.chunk.js"
+  },
+  {
+    "revision": "a6e10d0aeb38cad335c4",
+    "url": "/static/js/1.a6e10d0a.chunk.js"
+  },
+  {
+    "revision": "b528af72799e1122ed3e",
+    "url": "/static/js/2.b528af72.chunk.js"
+  },
+  {
+    "revision": "2aee90c1e04830ff2449",
+    "url": "/static/js/runtime~main.2aee90c1.js"
+  },
+  {
+    "revision": "2b2c7ed23adf1732af2c262c0d7dc86e",
+    "url": "/static/media/me.2b2c7ed2.jpeg"
+  },
+  {
+    "revision": "1718e52d68200458cddad05589d5db7f",
+    "url": "/index.html"
+  }
+];
